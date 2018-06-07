30543482a6d67fa967507ad985a4a6736217bc36
naturalele <38481595+naturalele@users.noreply.github.com>
2018-06-07 17:45:03 -0500

    Add files via upload

---
+++ memory_game/js/main.js
@@ -0,0 +1,23 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [ ];
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+var flipCard = function(cardId) {
+  {console.log("User flipped " + cards[cardId]);}
+  cardsInPlay.push("cardId");
+if (cardsInPlay.length === 2);
+else if (cardsInPlay[0] === cardsInPlay[1])
+	window.alert("You found a match!");
+	else if (cardsInPlay[0] !== cardsInPlay[1]); 
+		window.alert("Sorry, try again");
+	checkForMatch;
+	}
+
+flipCard(0);
+ flipCard(2);
+ 
